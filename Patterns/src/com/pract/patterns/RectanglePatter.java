9c389619c0a543055f50e06ca639e254294d28ca
Shubham Ramesh Patil <shubhu244248@gmail.com>
2023-12-24 12:42:53 +0530

    Rectangle Pattern Coding Ninja Stivert Sheet

---
+++ Patterns/src/com/pract/patterns/RectanglePatter.java
@@ -0,0 +1,21 @@
+package com.pract.patterns;
+
+import java.util.Scanner;
+
+public class RectanglePatter {
+	public static void nForest(int n) {
+		// Write your code here
+		for (int line = 1; line <= n; line++) {
+			for (int star = 1; star <= n; star++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		nForest(n);
+	}
+}
